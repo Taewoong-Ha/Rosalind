b04c3a7874661089d5f888437057c0897951cf71
Taewoong-Ha <61774079+Taewoong-Ha@users.noreply.github.com>
2022-10-30 17:49:28 +0900

    Update Open Reading Frames.py

--- Open Reading Frames.py
+++ Open Reading Frames.py
@@ -1,3 +1,4 @@
+# 파일 읽기
 f = open('/Users/hataewoong/Downloads/rosalind_orf.txt', 'r')
 lines = f.readlines()
 dna_seq = ""
@@ -8,6 +9,7 @@ for line in lines:
     else:
         dna_seq += line
 
+# reverse complement
 dna_seq_rev = ''
 for i in dna_seq[::-1]:
     if i == "G":
@@ -18,10 +20,11 @@ for i in dna_seq[::-1]:
         dna_seq_rev += "T"
     elif i == "T":
         dna_seq_rev += "A"
-        
+      
+# stop
 stop = ['TAA','TAG','TGA']
 
-
+# dna to protein dictionary
 dna_codon = {}
 f = open('dan_codon_table.txt', 'r')
 lines = f.readlines()
@@ -30,7 +33,7 @@ for line in lines:
     line[1] = line[1].split('\n')
     dna_codon [line[0]] = line[1][0]
     
-
+# atg index
 def ATG_index(seq):
     a = seq.find("ATG")
     atg_index = []
@@ -42,13 +45,15 @@ def ATG_index(seq):
         
     return atg_index
 
-
+# atg index 기반 protein translation
 def protin(index, seq):
     protein = ""
+    # 이중 포문 break 할 때 사용
     breaker = False
     break_point = "Stop"
-
+    # atg index 기반으로 seq을 codon으로 구분
     split_dna = list(map(''.join, zip(*[iter(seq[index:])] * 3)))
+    # codon에 stop codon이 존재하면 orf니까 진행하고 아니면 split_dna 자체를 제거
     for i in stop:
         if i in split_dna:
             for j in split_dna:
@@ -64,7 +69,8 @@ def protin(index, seq):
                 
         else: 
             split_dna = []
-
+            
+    # 여기는 protein seq 뒤에 Stop가 붙어서 그걸 떼놓기 위한 작업
     if protein[-4:] != "Stop":
         protein = ""
     else:
@@ -73,6 +79,7 @@ def protin(index, seq):
             
     return protein
 
+# atg index에 따라서 forward, reverse에 따라 protein 
 res = []
 for i in ATG_index(dna_seq):
     res.append(protein(i, dna_seq))
