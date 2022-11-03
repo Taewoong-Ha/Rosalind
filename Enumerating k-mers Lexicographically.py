c18fde8fd5ba74b1bd09e07bb0582c701d71153d
Taewoong-Ha <61774079+Taewoong-Ha@users.noreply.github.com>
2022-11-03 09:16:48 +0900

    Update Enumerating k-mers Lexicographically.py

--- Enumerating k-mers Lexicographically.py
+++ Enumerating k-mers Lexicographically.py
@@ -1,7 +1,11 @@
 from itertools import product
+# 불러오기
+f = open('C:/Users/user/Downloads/rosalind_lexf.txt','r')
+# 공백으로 분리해서 리스트 형태로 저장
+lines = f.read().split()
+string = lines[:len(lines)-1]
+length = int(lines[-1])
 
-string = ["A", "B", "C", "D", "E", "F", "G", "H"]
-length = 3
 
 for i in product(string, repeat = length):
     # print(i)
@@ -13,4 +17,6 @@ for i in product(string, repeat = length):
         else:
             # print(len(k))
             print(k)
+
+
 
