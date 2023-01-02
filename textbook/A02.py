da035e8a219189028b7dd9f8f54897af268bc0ac
Taewoong-Ha <61774079+Taewoong-Ha@users.noreply.github.com>
2023-01-02 21:59:42 +0900

    Update A02.py

--- textbook/A02.py
+++ textbook/A02.py
@@ -1,1 +1,27 @@
 
+def patterncount(text, pattern):
+    count = 0
+    for i in range(0, len(text) - len(pattern) +1):
+        if text[i:i + len(pattern)] == pattern:
+            count += 1
+  
+  
+  def count:
+    pass
+  
+  def frequentwords(text, k):
+    frequentpatterns = []
+    for i in range(0, len(text) - k + 1):
+        pattern = text[i:i+k]
+        count[i] = patterncount(text, pattern)
+    maxcount = max(count)
+    for i in range(0, len(text) - k + 1):
+        if count[i] == maxcount:
+            frequentpatterns.append(text[i:i+k])
+    list(set(frequentpatterns))
+    return frequentpatterns
+
+text = input()
+k = int(input())
+
+frequentwords(text, k)
